685c9e8df942efb110125f8eaeac01b64fa8af49
LiYingZe <3615267113@qq.com>
2025-01-05 15:51:12 +0800

    0105 Commit

--- DACA.py
+++ DACA.py
@@ -12,11 +12,9 @@ import argparse
 
 # make joint weightPart
 # Objective: Need to test the robustness of the attack method
-# STATS :  5-Tables
-# IMDB : 5-Tables
 
 def create_bidirectional_mapping(df, primary_key):
-    # 确保主键列存在
+    # Check primary key
     if primary_key not in df.columns:
         raise ValueError(f"The primary key '{primary_key}' does not exist in the DataFrame.")
     row_to_key = {}
@@ -27,11 +25,6 @@ def create_bidirectional_mapping(df, primary_key):
             print('\r', index, '/', rtol, end='', flush=True)
         row_to_key[index] = row[primary_key]
         key_to_row[row[primary_key]] = index
-    # 创建行号到主键值的映射
-    # row_to_key = {index: row[primary_key] for index, row in df.iterrows()}
-    # # 创建主键值到行号的映射
-    # key_to_row = {value: index for index, value in row_to_key.items()}
-
     return row_to_key, key_to_row
 
 
@@ -49,13 +42,11 @@ def evalDBCard(dbCSVPath='./CorruptDB/ours_stats_simplified_posts.csv_0.2/', dat
     qerrList = []
     file = open(qf, 'r')
     lines = file.readlines()
-    # print(name2pd.keys())
     shapedTabName = tableRx.replace('.csv', '')
     for idx, line in enumerate(lines):
         print('\rQid:', idx)
         cleaned_line = line.rstrip(';\n')
         print(cleaned_line)
-        # 下面的后面需要统一一下，这里有点问题
         sqlQuery = cleaned_line.split("||")[1]
         card = int(cleaned_line.split("||")[0])
         clause = (sqlQuery.split("FROM")[1].split("WHERE")[0]).strip()
@@ -63,7 +54,6 @@ def evalDBCard(dbCSVPath='./CorruptDB/ours_stats_simplified_posts.csv_0.2/', dat
         abrevTab = None
         if tableRx.replace('.csv', '') in clause:
             inTab = True
-            # ABBREV
             if dataBase == "stats_simplified":
                 abbrev = {'posts': 'p', 'tags': 't', 'users': 'u', 'postHistory': 'ph', 'badges': 'b', 'votes': 'v',
                           'postLinks': 'pl', 'comments': 'c'}
@@ -73,24 +63,15 @@ def evalDBCard(dbCSVPath='./CorruptDB/ours_stats_simplified_posts.csv_0.2/', dat
             qerrList.append(max((cardNew + 1) / (card + 1), (card + 1) / (cardNew + 1)))
             print('Nonzero', len(qerrList), "QERROR:", np.percentile(qerrList, [50, 90, 95, 99, 100]))
 
-            # exit(1)
-            # sqlQuery = sqlQuery.replace(" as")
-
 
 def getJointWeight(dataBase='stats_simplified', tableRx='posts.csv', query='stats_CEB.sql', primaryKey="Id"):
     objectivePath = r'./WeightCache/' + dataBase + tableRx.replace('.', 'D') + query.replace('.', 'D') + primaryKey
     if os.path.exists(objectivePath + '.npy'):
-        print(f"文件 '{objectivePath}' 存在。")
+        print(f"File '{objectivePath}' Exist!")
         return np.load(objectivePath + '.npy')
     else:
         print(objectivePath)
         print("Cache FileNotFind")
-    # print(objectivePath)
-    # exit(1)
-
-    # 计算多表连接权重
-    # 读取文件并处理每一行
-    # 遍历文件夹中的所有文件
     con = duckdb.connect()
     name2pd = {}
     print("Registering")
@@ -104,20 +85,11 @@ def getJointWeight(dataBase='stats_simplified', tableRx='posts.csv', query='stat
     qf = './query/' + query
     t1 = time.time() - t0
     print("Register takes", t1)
-    # sqlT = "SELECT t.id::INTEGER, COUNT(*)  FROM movie_companies mc,title t,movie_info_idx mi_idx WHERE t.id=mc.movie_id AND t.id=mi_idx.movie_id AND mi_idx.info_type_id=112 AND mc.company_type_id=2 GROUP BY t.id::INTEGER;"
-    # # print('after:', sqlT)
-    # VALS = con.execute(sqlT)
-    # VALS = VALS.df().to_numpy()
-    # r, c = VALS.shape
-    # print(VALS)
-    #
-    # exit(1)
     qerrList = []
     file = open(qf, 'r')
     # with open(qf, 'r') as file:
     lines = file.readlines()
     objTab = name2pd[tableRx.replace('.csv', '')]
-    # 构造pk2rid的双射
     print("Making bidirectional_mapping from pk2rid", flush=True)
     t0 = time.time()
     row_to_key, key_to_row = create_bidirectional_mapping(objTab, primaryKey)
@@ -126,8 +98,6 @@ def getJointWeight(dataBase='stats_simplified', tableRx='posts.csv', query='stat
     print(len(row_to_key.keys()))
     print(len(key_to_row.keys()))
     print("Done", flush=True)
-    # pk2rid =
-    # exit(1)
     rowNumber = int(len(key_to_row.keys()))
     weightColumn = np.zeros((rowNumber, len(lines)))
     print(weightColumn.shape)
@@ -136,7 +106,6 @@ def getJointWeight(dataBase='stats_simplified', tableRx='posts.csv', query='stat
         print('\rQid:', idx)
         cleaned_line = line.rstrip(';\n')
         print(cleaned_line)
-        # 下面的后面需要统一一下，这里有点问题
         if 'job' in dataBase:
             sqlQuery = cleaned_line
         else:
@@ -166,7 +135,6 @@ def getJointWeight(dataBase='stats_simplified', tableRx='posts.csv', query='stat
             else:
                 sqlQuery = sqlQuery.replace('COUNT(*)', abrevTab + '.Id, COUNT(*) ').replace(";",
                                                                                              '') + ' GROUP BY ' + abrevTab + '.Id;'
-            # sqlQuery = sqlQuery.replace(" as")
             print(sqlQuery)
             VALS = con.execute(sqlQuery)
             # print(VALS)
@@ -176,7 +144,6 @@ def getJointWeight(dataBase='stats_simplified', tableRx='posts.csv', query='stat
                 pkValue = VALS[i, 0]
                 rid = key_to_row[pkValue]
                 weightColumn[rid, idx] += (VALS[i, 1])
-            # print(sum(weightColumn[:,idx]),card)
         else:
             continue
     np.save(objectivePath, weightColumn)
@@ -230,12 +197,9 @@ def greedMaxVGPU(rij, ratio):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     rij = torch.tensor(rij, dtype=torch.float32).to(device)
     cardOld = torch.tensor(cardOld, dtype=torch.float32).to(device)
-    # 将vector转换为PyTorch张量，并移动到GPU上
     vector = torch.ones(rij.shape[0], device='cuda')
-    # 初始化cardOld和cardEst为PyTorch张量，并移动到GPU上
     cardOld = getCard(rij, vector)
     cardEst = cardOld.clone().detach()
-    # 将gainIfDelete转换为PyTorch张量，并移动到GPU上
     gainIfDelete = (((cardOld + 1) / (cardEst + 1 - rij)) - 1).sum(dim=1)
     gainIfIns = (((cardOld + 1 + rij) / (cardEst + 1)) - 1).sum(dim=1)
     cardEst_last = cardOld.clone().detach()
@@ -257,26 +221,26 @@ def greedMaxVGPU(rij, ratio):
         tx = time.time()
         gainIfDelete = (cardEst / (cardEst + 1 - rij * (mask))).sum(dim=1)
         gainIfIns = ((cardEst + rij * (mask) ) / (cardOld + 1)).sum(dim=1)
-        gainIfDelete = gainIfDelete * vector  # 相当于R <- R\v
+        gainIfDelete = gainIfDelete * vector   
         gainIfIns = gainIfIns * vector
         t1 = time.time()
         if iter % 100 == 0:
             print(iter, '/', int(ratio * rij.shape[0]))
             print("ETA:", (t1 - t0) * ratio * rij.shape[0] / 60.0, 'min')
-            vecQerr(cardOld, cardEst)  # 确保这个函数也可以处理PyTorch张量
-    return vector.cpu().numpy()  # 如果需要返回NumPy数组，将张量移回CPU
+            vecQerr(cardOld, cardEst)  
+    return vector.cpu().numpy()  
 
 
 class Attacker:
     def __init__(self, ratio=0.2, dataBase='stats_simplified', tableRx='posts.csv', query='stats_CEB.sql',
                  primaryKey="Id"):
         """
-        攻击者集成
-        :param ratio: 破坏的比例
-        :param dataBase: 数据库
-        :param tableRx: 被破坏的表
-        :param query: 给定的查询负载
-        :param primaryKey: 主键 STATS:Id     IMDB:id  注意大小写
+        Attacker Integration
+        :param ratio: The proportion of disruption
+        :param dataBase: The database
+        :param tableRx: The table to be disrupted
+        :param query: The given query load
+        :param primaryKey: Primary key STATS:Id     IMDB:id  Note the case sensitivity
         """
         self.ratio = ratio
         self.dataBase = dataBase
@@ -292,14 +256,10 @@ class Attacker:
         if method == "ours":
             path = r'./WeightCache/Ours_' + str(self.ratio) + self.DB_TABLE_QUERY_pk_Name
             if os.path.exists(path + '.npy'):
-                # 首先检查是否算过了攻击向量
                 atkVec = np.load(path + '.npy').reshape(1, -1)
                 cardOld = np.ones((1, atkVec.shape[1])) @ self.rij
                 cardNew = atkVec @ self.rij
-                # vecQerrcpu(cardOld, cardNew)
                 vecQerrcpu(cardOld, cardNew)
-                # 如果没有算过，重新生成
-                return
             else:
                 t0 = time.time()
                 print("Generation attack vector")
@@ -324,14 +284,9 @@ class Attacker:
 
         elif method == "random":
             atkVec = np.ones((1, self.rij.shape[0]))
-            # 计算总共有多少个元素
             total_elements = self.rij.shape[0]
-            # 计算20%的元素数量
             num_zeros = int(total_elements * self.ratio)
-            # 随机生成num_zeros个索引
             indices_to_zero = np.random.choice(total_elements, num_zeros, replace=False)
-            # 将这些索引对应的元素置为零
-            # 因为atkVec是一个二维数组，我们需要将其展平为一维数组进行操作
             atkVec = atkVec.reshape(-1)
             atkVec[indices_to_zero] = 0
             atkVec = atkVec.reshape(1, -1)
@@ -339,7 +294,7 @@ class Attacker:
             cardNew = atkVec @ self.rij
             vecQerrcpu(cardOld, cardNew)
 
-        # 输出数据库
+        #Make Corrupt DB
         out_Path = './CorruptDB/' + method + '_' + self.dataBase + '_' + self.tableRx + "_" + str(self.ratio) + '/'
         srcDB_Path = f"./datasets/" + self.dataBase + '/'
         if not os.path.exists(out_Path):
